51c4d9907f8043f7f56f7c88edd87fa76a0fc4cf
kirixen <robloxgamer3300373@gmail.com>
2025-04-15 19:48:49 +0530

    test

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,7 +8,7 @@ function Dashboard({ userData }) {
   const dropdownRef = useRef(null);
 
   useEffect(() => {
-    axios.get('https://arona-backend.vercel.app//api/bot-stats', { withCredentials: true })
+    axios.get('https://arona-backend.vercel.app/api/bot-stats', { withCredentials: true })
       .then(response => setBotStats(response.data))
       .catch(error => console.error('Error fetching bot stats:', error));
 
